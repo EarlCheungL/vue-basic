20d6321da2eae4cc75e162ff9f562acc56a4bbf1
EarlCheungL <EarlCheungL@gmail.com>
2019-05-23 23:31:25 +0800

    demo personblog

---
+++ my-blog/src/components/SingleBlog.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="single-blog">
+    <h1>{{blog.title}}</h1>
+    <article>{{blog.content}}</article>
+    <p>作者:{{blog.author}}</p>
+    <p>分类:</p>
+    <ul>
+      <li v-for="category in blog.categories" :key="category">
+        {{category}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"single-blog",
+  data(){
+    return{
+      id:this.$route.params.id,
+      blog:{}
+    }
+  },
+  created(){
+      this.$http.get("https://vue-basic-8ede5.firebaseio.com/posts/"
+       +this.id+".json")
+      .then(function(data){
+        return data.json();
+      })
+      .then(function(data){
+        this.blog=data;
+      })
+    }
+}
+</script>
+
+<style scoped>
+  #single-blog{
+    max-width: 960px;
+    margin: 0 auto;
+    padding:20px;
+    background: grey;
+    border:1px dotted black;
+  }
+h1{
+  text-transform : uppercase;
+}
+</style>
---
+++ my-blog/src/main.js
@@ -0,0 +1,53 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+import App from './App'
+import VueRouter from 'vue-router'
+import Routes from './routes'
+
+Vue.config.productionTip = false
+
+Vue.use(VueResource)
+Vue.use(VueRouter)
+
+Vue.directive('rainbow', {
+    bind(el, binding, vnode) {
+        el.style.color = "#" + Math.random().toString(16).slice(2, 8);
+    }
+})
+
+Vue.directive('theme', {
+    bind(el, binding, vnode) {
+        if (binding.value == 'wide') {
+            el.style.maxWidth = "1200px";
+        } else if (binding.value == 'narrow') {
+            el.style.maxWidth = "560px";
+        }
+
+        if (binding.arg == 'column') {
+            el.style.background = "#999999";
+            el.style.padding = '20px';
+        }
+    }
+})
+
+Vue.filter("to-uppercase", function(value) {
+    return value.toUpperCase();
+})
+Vue.filter("snippet", function(value) {
+    return value.slice(0, 100) + "...";
+})
+
+const router = new VueRouter({
+    routes: Routes,
+    mode: "history"
+})
+
+/* eslint-disable no-new */
+new Vue({
+    el: '#app',
+    components: { App },
+    template: '<App/>',
+    router: router
+})
